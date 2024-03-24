2030641bf6cd535b4cfedc31c2fdef91da8eb745
max hps <164808879+maxhps@users.noreply.github.com>
2024-03-25 05:58:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6bb380f3-c4b1-4a98-b8af-b6f33babb814/)
 
 
 echo "$curl_output" >> Update.tsx
